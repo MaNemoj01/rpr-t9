fdc2d544c06a648c7cf59fa51847b2fd9cbe175f
MaNemoj01 <44163914+MaNemoj01@users.noreply.github.com>
2018-12-24 15:31:36 +0100

    implementirana drzava

--- src/ba/unsa/etf/rpr/Drzava.java
+++ src/ba/unsa/etf/rpr/Drzava.java
@@ -1,4 +1,34 @@
 package ba.unsa.etf.rpr;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 public class Drzava {
+    private SimpleIntegerProperty id= new SimpleIntegerProperty(0);
+    private SimpleStringProperty naziv = new SimpleStringProperty("");
+    private Grad glavni_grad;
+
+    public Drzava() {}
+    public Drzava(int id, String naziv, int glavni_grad) {
+        this.id=new SimpleIntegerProperty(id);
+        this.naziv=new SimpleStringProperty(naziv);
+        // this.glavni_grad.setNaziv(glavni_grad);
+    }
+
+
+    public void setGlavniGrad(Grad glavni_grad) {
+        this.setGlavni_grad(glavni_grad);
+    }
+
+    public SimpleIntegerProperty idProperty() {return id; }
+    public int getId() { return id.get();}
+    public void setId(int id) { this.id.set(id); }
+
+    public String getNaziv() { return naziv.get(); }
+    public SimpleStringProperty nazivProperty() { return naziv; }
+    public void setNaziv(String naziv) { this.naziv.set(naziv); }
+
+
+    public Grad getGlavni_grad() { return glavni_grad; }
+    public void setGlavni_grad(Grad glavni_grad) { this.glavni_grad = glavni_grad; }
 }
